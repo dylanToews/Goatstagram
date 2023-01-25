65d9633a7cac461cda728f757f5e3b8a214a68fc
dylanToews <dylan.toews@yahoo.ca>
2023-01-25 15:17:26 -0700

    Story component created and roughly styled. Mock data added for users

---
+++ data/users.js
@@ -0,0 +1,28 @@
+export const USERS = [
+  {
+    user: 'Billy the Kid',
+    image: 'https://media.npr.org/assets/img/2014/12/14/ap798386886673_custom-247a20518bf04f86ef4457d12939f46521c8751b-s800-c85.webp',
+  },
+  {
+    user: 'Butt-Head',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/2/23/Mountain_Goat%2C_Enchantments_Basin.jpg',
+  },
+  {
+    user: 'Scape Goat',
+    image: 'https://images.unsplash.com/photo-1524024973431-2ad916746881?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z29hdHxlbnwwfHwwfHw%3D&w=1000&q=80',
+  },
+  {
+    user: 'Vincent van Goat',
+    image: 'https://media.istockphoto.com/id/531973734/photo/portrait-of-a-black-goat-on-a-black-background.jpg?s=612x612&w=0&k=20&c=AIu6zF4RxyO1J8yvkbg4ZnhPV9iiN81Ohwp62PBc7yU=',
+  },
+  {
+    user: 'Butt-Head',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/2/23/Mountain_Goat%2C_Enchantments_Basin.jpg',
+  },
+  {
+    user: 'Scape Goat',
+    image: 'https://images.unsplash.com/photo-1524024973431-2ad916746881?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Z29hdHxlbnwwfHwwfHw%3D&w=1000&q=80',
+  }
+
+  
+]
