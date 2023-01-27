acc2874bc931f705b5e60557642af45576651448
dylanToews <dylan.toews@yahoo.ca>
2023-01-27 12:31:52 -0700

    bottom tabs added and styled

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,6 +4,7 @@ import Header from '../components/home/Header';
 import Stories from '../components/home/Stories';
 import Post from '../components/home/Post';
 import {POSTS} from '../data/posts';
+import BottomTabs, { bottomTabIcons } from '../components/home/BottomTabs';
 
 const HomeScreen = () => {
   return (
@@ -19,6 +20,7 @@ const HomeScreen = () => {
 
         ))}
       </ScrollView>
+      <BottomTabs icons={bottomTabIcons}/>
     </SafeAreaView>
   )
 }
